3ba24b62124ce71e3aff4080bdd326d814367db3
Joseph <jnjbros2@gmail.com>
2016-10-31 13:55:31 -0700

    tutorial.scene
    *added half the tutorial
    *added sky box

---
+++ UnityGame/Assets/door_falls.js
@@ -0,0 +1,29 @@
+﻿#pragma strict
+public var inArea : boolean = false;
+public var door : GameObject;
+public var player : GameObject;
+public var doorIsOpen : boolean = false;
+static var brokeTheDoor : boolean = false;
+function Start () {
+
+}
+
+function Update () {
+    if(Input.GetButtonDown("Use")){
+        if(inArea){
+            if(doorIsOpen==false){
+                door.GetComponent(Animation).Play ("door fall");
+                door_falls.brokeTheDoor = true;
+                doorIsOpen=false;
+            }
+        }
+    }
+}
+
+function OnTriggerEnter (player : Collider){
+    inArea=true;
+}
+
+function OnTriggerExit (player : Collider){
+    inArea=false;
+}
